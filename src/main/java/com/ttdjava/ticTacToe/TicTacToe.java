164b70094015aa96f8239f285eed5174e9781fa8
Igor Nosovskyi <nosovskyigor@gmail.com>
2019-01-31 14:00:32 +0100

    logic first player

--- src/main/java/com/ttdjava/ticTacToe/TicTacToe.java
+++ src/main/java/com/ttdjava/ticTacToe/TicTacToe.java
@@ -3,19 +3,33 @@ package com.ttdjava.ticTacToe;
 public class TicTacToe {
 
     private Character [] [] board =  {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
-
+    private String lastPlayer = null;
 
     public void play(int x, int y) {
-        if (x < 1 || x > 3) {
-            throw new RuntimeException("X is outside board");
+        checkAxis(x);
+        checkAxis(y);
+        setBoard(x, y);
+        lastPlayer = nextPlayer();
+    }
+
+    public void checkAxis (int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException(axis + " is outside board");
         }
-        else if (y < 1 || y > 3) {
-            throw new RuntimeException("Y is outside board");
-        }
+    }
+
+    public void setBoard (int x, int y) {
         if (board[x - 1][y - 1] != '\0') {
             throw new RuntimeException("Box is occupied");
         } else {
             board[x - 1][y - 1] = 'X';
         }
+    }
+
+    public String nextPlayer() {
+        if (lastPlayer == "X") {
+            return "O";
+        }
+        return "X";
     }
 }
--- src/test/java/com/ttdjava/ticTacToe/TicTacToeSpecTest.java
+++ src/test/java/com/ttdjava/ticTacToe/TicTacToeSpecTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 public class TicTacToeSpecTest {
 
@@ -28,10 +29,22 @@ public class TicTacToeSpecTest {
         exception.expect(RuntimeException.class);
         ticTacToe.play(2,5);
     }
+
     @Test
     public void whenOccupiedThenRuntimeException() {
         ticTacToe.play(2, 1);
         exception.expect(RuntimeException.class);
         ticTacToe.play(2, 1);
+    }
+
+    @Test
+    public void givenFirstTurnWhenNextPlayerThenX(){
+        assertEquals("X",ticTacToe.nextPlayer());
+    }
+
+    @Test
+    public void givenLastTurnWasXWhenNextPlayerThenO(){
+        ticTacToe.play(1,1);
+        assertEquals("O",ticTacToe.nextPlayer());
     }
 }
