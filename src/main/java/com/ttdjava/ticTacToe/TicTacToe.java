a5ca2adc02824263b91521ef7599dc17682c44d3
igor.nosovskyi <nosovskyigor@gmail.com>
2019-03-04 15:43:44 +0100

    Add test info page 67

--- src/main/java/com/ttdjava/ticTacToe/TicTacToe.java
+++ src/main/java/com/ttdjava/ticTacToe/TicTacToe.java
@@ -2,23 +2,51 @@ package com.ttdjava.ticTacToe;
 
 public class TicTacToe {
 
-    private Character [] [] board =  {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
-    private String lastPlayer = null;
+    private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
 
-    public void play(int x, int y) {
+    private char lastPlayer = '\0';
+    private static final int SIZE = 3;
+    //TODO page 67
+    public String play(int x, int y) {
         checkAxis(x);
         checkAxis(y);
         setBoard(x, y);
         lastPlayer = nextPlayer();
+        setBox(x, y, lastPlayer);
+
+
+        if (isWin()) {
+            return lastPlayer + " is the winner";
+        }
+        return "No winner";
     }
 
-    public void checkAxis (int axis) {
+    private boolean isWin() {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] + board[1][i] + board[2][i]
+                    == (lastPlayer * SIZE)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void setBox(int x, int y, char lastPlayer) {
+        if (board[x - 1][y - 1] != '\0') {
+            throw
+                    new RuntimeException("Box is occupied");
+        } else {
+            board[x - 1][y - 1] = lastPlayer;
+        }
+    }
+
+    public void checkAxis(int axis) {
         if (axis < 1 || axis > 3) {
             throw new RuntimeException(axis + " is outside board");
         }
     }
 
-    public void setBoard (int x, int y) {
+    public void setBoard(int x, int y) {
         if (board[x - 1][y - 1] != '\0') {
             throw new RuntimeException("Box is occupied");
         } else {
@@ -26,10 +54,10 @@ public class TicTacToe {
         }
     }
 
-    public String nextPlayer() {
-        if (lastPlayer == "X") {
-            return "O";
+    public char nextPlayer() {
+        if (lastPlayer == 'X') {
+            return 'O';
         }
-        return "X";
+        return 'X';
     }
 }
